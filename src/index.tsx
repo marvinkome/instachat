3301b8dea16994ceb8545b0c460f0c73f8419835
marvinkome <marvinkome@gmail.com>
2018-10-24 00:29:49 +0100

    refactor project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,54 @@
 import * as React from 'react';
 import { YellowBox } from 'react-native';
-import Screen from './screens';
+import { AsyncStorage } from 'react-native';
+import { MessageBar, MessageBarManager } from 'react-native-message-bar';
+import rootNavigator from './screens';
 
-YellowBox.ignoreWarnings([
-    'Warning: isMounted(...) is deprecated',
-    'Module RCTImageLoader'
-]);
-
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 console.ignoredYellowBox = ['Setting a timer'];
 
-export default class App extends React.Component {
+interface IState {
+    isLoaded: boolean;
+    isLoggedIn: boolean;
+}
+
+export default class App extends React.Component<{}, IState> {
+    alert = React.createRef();
+    state = {
+        isLoaded: false,
+        isLoggedIn: false
+    };
+
+    async componentDidMount() {
+        const token = await AsyncStorage.getItem('client_id');
+        const isLoggedIn = token !== null;
+
+        this.setState({
+            isLoaded: true,
+            isLoggedIn
+        });
+
+        MessageBarManager.registerMessageBar(this.alert.current);
+    }
+
+    componentWillUnmount() {
+        MessageBarManager.unregisterMessageBar();
+    }
+
     render() {
-        return <Screen />;
+        const { isLoaded, isLoggedIn } = this.state;
+
+        if (isLoaded) {
+            const Navigator = isLoggedIn ? rootNavigator('Main') : rootNavigator('Login');
+
+            return (
+                <React.Fragment>
+                    <Navigator />
+                    <MessageBar ref={this.alert} />
+                </React.Fragment>
+            );
+        }
+
+        return null;
     }
 }
--- src/screens/main/navigator.tsx
+++ src/screens/main/navigator.tsx
@@ -1,11 +1,11 @@
 import { createStackNavigator } from 'react-navigation';
 
 // routes
-import Home from './../home';
-import Chat from './../chat';
-import NewGroup from './../newGroup';
-import JoinGroup from './../joinGroup';
-import EditProfile from './../editProfile';
+import Home from './home';
+import Chat from './chat';
+import NewGroup from './newGroup';
+import JoinGroup from './joinGroup';
+import EditProfile from './editProfile';
 
 export default createStackNavigator(
     {
